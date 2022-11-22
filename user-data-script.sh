8d3df34d7cd11c3a93a26c135b8a18e8deba5f85
Ray Mileo <ray.mileo@turbonomic.com>
2022-11-22 10:01:32 -0500

    Upgrade to stress-ng

--- user-data-script.sh
+++ user-data-script.sh
@@ -4,11 +4,11 @@ dnf config-manager --set-enabled crb
 dnf install epel-release epel-next-release -y
 dnf check-update
 # dnf upgrade -y
-dnf install stress -y 
+dnf install stress-ng -y 
 
 cat > /usr/sbin/stress_start.sh <<EOF
 #!/bin/bash
-stress --cpu 8 
+stress-ng --cpu 8 
 EOF
 
 cat > /etc/systemd/system/stress.service <<EOF
