fe0fae4f836a12f29f0a1d2fa3ba2f9f049c33fa
Ray Mileo <ray.mileo@turbonomic.com>
2022-11-29 11:05:34 -0500

    Modify AMI to use Amazon Linux 2 image

--- user-data-script.sh
+++ user-data-script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-dnf config-manager --set-enabled crb
-dnf install epel-release epel-next-release -y
-dnf check-update
-# dnf upgrade -y
-dnf install stress-ng -y 
+
+amazon-linux-extras install -y epel
+yum -y update
+yum install stress-ng -y 
 
 cat > /usr/sbin/stress_start.sh <<EOF
 #!/bin/bash
